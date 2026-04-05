b9cd4343fb6bcc92c89249665b0abcdb39b76664
rahul tanpure <tanpurer909@gmail.com>
2026-04-05 15:58:42 +0530

    update: replace Netlify domain with Vercel domain in CORS configuration

--- src/main/java/com/interviewpro/interviewpro/configuration/CorsConfig.java
+++ src/main/java/com/interviewpro/interviewpro/configuration/CorsConfig.java
@@ -18,7 +18,7 @@ public class CorsConfig {
 
         config.setAllowedOrigins(List.of(
                 "http://localhost:4200",
-                "https://interview-prepration-pro.netlify.app"
+                "https://interview-pro-frontend.vercel.app"
         ));
         config.setAllowedMethods(List.of(
                 "GET", "POST", "PUT", "DELETE", "OPTIONS"
